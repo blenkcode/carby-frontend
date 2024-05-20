408e92c4d9b710225e69daef2a9035d0efc4d059
Pierre SIMON <pierresimonnancy@me.com>
2024-05-20 12:13:59 +0200

    test2

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import { View, TextInput, ImageBackground, StyleSheet, Text, TouchableOpacity, KeyboardAvoidingView, Platform} from 'react-native';
-import { useState } from 'react';
+import React from "react";
+import {
+  View,
+  TextInput,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import { useState } from "react";
 
-const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const EMAIL_REGEX =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export default function SignUpScreen({ navigation }) {
   const [username, setUsername] = useState("");
@@ -10,77 +20,86 @@ export default function SignUpScreen({ navigation }) {
   const [email, setEmail] = useState("");
   const [emailError, setEmailError] = useState(false);
 
-    const handleSubmit = () => {
-        if (EMAIL_REGEX.test(email)) {
-          navigation.navigate('SignIn');
-        } else {
-          setEmailError(true);
-        }
-      });
+  const handleSubmit = () => {
+    if (EMAIL_REGEX.test(email)) {
+      navigation.navigate("SignIn");
+    } else {
+      setEmailError(true);
+    }
   };
-
-  return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require("../assets/SignUp.png")}
-        style={styles.backgroundImage}
-      >
-        <View style={styles.titleContainer}>
-          <View style={styles.logoContainer}>
-            <Text style={styles.title}>CARBY</Text>
-          </View>
-        </View>
-
-        <KeyboardAvoidingView
-          style={styles.inputContainer}
-          behavior={Platform.OS === "ios" ? "padding" : "height"}
-        >
-          <View style={styles.inputsContainer}>
-            <View style={styles.inputWrapper}>
-              <Text style={styles.label}>Nom d'utilisateur</Text>
-              <TextInput
-                style={styles.input}
-                onChangeText={(value) => setUsername(value)}
-                value={username}
-              />
-            </View>
-
-                      <View style={styles.inputWrapper}>
-                          <Text style={styles.label}>Email</Text>
-                          <TextInput style={styles.input} autoCapitalize="none" keyboardType="email-address" textContentType="emailAddress" autoComplete="email" onChangeText={(value) => setEmail(value)} value={email}/>
-                          {emailError && <Text style={styles.error}>Invalid email address</Text>}
-                      </View>
-
-            <View style={styles.inputWrapper}>
-              <Text style={styles.label}>Mot de passe</Text>
-              <TextInput
-                style={styles.input}
-                onChangeText={(value) => setPassword(value)}
-                value={password}
-                secureTextEntry
-              />
-            </View>
-          </View>
-        </KeyboardAvoidingView>
-
-        <View style={styles.btn1Flex}>
-          <TouchableOpacity style={styles.btn} onPress={() => handleSubmit()}>
-            <Text style={styles.btnText}>C'est parti !</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.btn2Flex}>
-          <TouchableOpacity
-            style={styles.btn2}
-            onPress={() => navigation.navigate("SignIn")}
-          >
-            <Text style={styles.btnText2}>J'ai déjà un compte</Text>
-          </TouchableOpacity>
-        </View>
-      </ImageBackground>
-    </View>
-  );
 }
+
+return (
+  <View style={styles.container}>
+    <ImageBackground
+      source={require("../assets/SignUp.png")}
+      style={styles.backgroundImage}
+    >
+      <View style={styles.titleContainer}>
+        <View style={styles.logoContainer}>
+          <Text style={styles.title}>CARBY</Text>
+        </View>
+      </View>
+
+      <KeyboardAvoidingView
+        style={styles.inputContainer}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <View style={styles.inputsContainer}>
+          <View style={styles.inputWrapper}>
+            <Text style={styles.label}>Nom d'utilisateur</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={(value) => setUsername(value)}
+              value={username}
+            />
+          </View>
+
+          <View style={styles.inputWrapper}>
+            <Text style={styles.label}>Email</Text>
+            <TextInput
+              style={styles.input}
+              autoCapitalize="none"
+              keyboardType="email-address"
+              textContentType="emailAddress"
+              autoComplete="email"
+              onChangeText={(value) => setEmail(value)}
+              value={email}
+            />
+            {emailError && (
+              <Text style={styles.error}>Invalid email address</Text>
+            )}
+          </View>
+
+          <View style={styles.inputWrapper}>
+            <Text style={styles.label}>Mot de passe</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={(value) => setPassword(value)}
+              value={password}
+              secureTextEntry
+            />
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+
+      <View style={styles.btn1Flex}>
+        <TouchableOpacity style={styles.btn} onPress={() => handleSubmit()}>
+          <Text style={styles.btnText}>C'est parti !</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.btn2Flex}>
+        <TouchableOpacity
+          style={styles.btn2}
+          onPress={() => navigation.navigate("SignIn")}
+        >
+          <Text style={styles.btnText2}>J'ai déjà un compte</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
