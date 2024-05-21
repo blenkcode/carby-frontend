16b22db260914c3219b750d74c8a0f51e6f630ae
Your name <Your email>
2024-05-21 16:09:06 +0200

    ArticlesScreen

--- screens/ArticlesScreen.js
+++ screens/ArticlesScreen.js
@@ -3,21 +3,21 @@ import { useEffect, useState } from 'react';
 
 export default function ArticlesScreen() {
   const [articlesData, setArticlesData] = useState([]);
+  const [expandedArticles, setExpandedArticles] = useState({});
 
   useEffect(() => {
     fetch('http://localhost:3000/articles/articles')
       .then(response => response.json())
       .then(data => {
-        console.log(data.articles);
         setArticlesData(data.articles);
       });
   }, []);
 
-  const truncateText = (text, maxLength) => {
-    if (text.length > maxLength) {
-      return text.substring(0, maxLength) + '... ';
-    }
-    return text;
+  const toggleReadMore = (index) => {
+    setExpandedArticles((prev) => ({
+      ...prev,
+      [index]: !prev[index],
+    }));
   };
 
   const handleReadMore = (url) => {
@@ -26,20 +26,28 @@ export default function ArticlesScreen() {
 
   const renderArticles = articlesData.map((article, index) => {
     if (!article.urlToImage) {
-      return null; // Skip rendering if there is no image
+      return null; // Si pas d'image ne pas afficher l'article
     }
+    const isExpanded = expandedArticles[index];
+    const content = isExpanded ? article.content : truncateText(article.content, 100);
+
     return (
       <View key={index} style={styles.articleContainer}>
         <Image source={{ uri: article.urlToImage }} style={styles.articleImage} />
         <Text style={styles.articleTitle}>{article.title}</Text>
         <Text style={styles.articleContent}>
-          {truncateText(article.content, 100)}
+          {content}
           {article.content.length > 100 && (
-            <TouchableOpacity onPress={() => handleReadMore(article.url)}>
-              <Text style={styles.readMoreText}>lire la suite</Text>
+            <TouchableOpacity onPress={() => toggleReadMore(index)}>
+              <Text style={styles.readMoreText}>{isExpanded ? 'lire moins' : 'lire la suite'}</Text>
             </TouchableOpacity>
           )}
         </Text>
+        {isExpanded && (
+          <TouchableOpacity onPress={() => handleReadMore(article.url)}>
+            <Text style={styles.readFullArticleText}>Lire l'article complet</Text>
+          </TouchableOpacity>
+        )}
         <Text style={styles.articleAuthor}>{article.author}</Text>
       </View>
     );
@@ -47,20 +55,30 @@ export default function ArticlesScreen() {
 
   return (
     <View style={styles.containerG}>
-    <ScrollView contentContainerStyle={styles.container}>
-      {renderArticles}
-    </ScrollView>
+      <ScrollView contentContainerStyle={styles.container}>
+        {renderArticles}
+      </ScrollView>
     </View>
   );
 }
 
+const truncateText = (text, maxLength) => {
+  if (text.includes('[+')) {
+    return text.split('[+')[0] + '... ';
+  }
+  if (text.length > maxLength) {
+    return text.substring(0, maxLength) + '... ';
+  }
+  return text;
+};
+
 const styles = StyleSheet.create({
-    containerG: {
-      flex: 1,
-      backgroundColor: '#3F5FFF',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
+  containerG: {
+    flex: 1,
+    backgroundColor: '#3F5FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   container: {
     flexGrow: 1,
     padding: 20,
@@ -74,6 +92,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFDF3F',
     padding: 15,
     borderRadius: 10,
+    shadowOffset: { width: 0, height: 7 },
+    shadowColor: 'black',
+    shadowOpacity: 0.25,
+    shadowRadius: 2,
+    elevation: 5,
   },
   articleImage: {
     width: '100%',
@@ -95,5 +118,16 @@ const styles = StyleSheet.create({
   articleContent: {
     textAlign: 'justify',
     fontSize: 16,
+  },
+  readMoreText: {
+    color: '#3F5FFF',
+    fontWeight: 'bold',
+    textAlign: 'right',
+  },
+  readFullArticleText: {
+    color: '#FF4500',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 10,
   },
 });
