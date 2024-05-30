ea26c00b3f644164a5a23ef49d27b13af06129e2
Pierre SIMON <pierresimonnancy@me.com>
2024-05-30 09:28:49 +0200

    final pierre

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { StyleSheet } from "react-native";
-import EyesIcon from "./assets/EyesIcon"; // Importer le composant SVG
+import EyesIcon from "./assets/eyesIcon"; // Importer le composant SVG
 
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
